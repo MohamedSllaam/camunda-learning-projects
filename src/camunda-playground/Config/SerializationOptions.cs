345b8f65c2e8b3fa0e983d1eecff357f969dad9e
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2026-01-20 01:17:33 +0200

    small

--- src/camunda-playground/Config/SerializationOptions.cs
+++ src/camunda-playground/Config/SerializationOptions.cs
@@ -1,11 +1,7 @@
-﻿using System.Text.Json.Serialization.Metadata;
+﻿namespace camunda_playground.Config;
 
-namespace camunda_playground.Config;
-
-using System.Text.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 
 public static class SerializationOptions
 {
